6e043fd6cba11d4d59011de91bea055f158142f4
HakJu <gkrwnrh@gmail.com>
2021-07-19 15:06:37 +0900

    [#3] feat: 마이그레이션 작성

--- Sources/App/Migrations/CreateProduct.swift
+++ Sources/App/Migrations/CreateProduct.swift
@@ -3,13 +3,18 @@ import SQLKit
 
 struct CreateProduct: Migration {
     func prepare(on database: Database) -> EventLoopFuture<Void> {
-        return database.schema("todos")
+        return database.schema(Products.schema)
             .id()
             .field("title", .string, .required)
+            .field("price", .uint64, .required)
+            .field("stock", .uint64, .required)
+            .field("description", .string)
+            .field("image", .array(of: .string))
+            .field("password", .string, .required)
             .create()
     }
 
     func revert(on database: Database) -> EventLoopFuture<Void> {
-        return database.schema("todos").delete()
+        return database.schema(Products.schema).delete()
     }
 }
